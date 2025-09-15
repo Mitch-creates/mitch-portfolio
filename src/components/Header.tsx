6ec29db4f5cb8e16d73a2c40d7dab8ed0e5e698a
Mitch-creates <mitchcreates@proton.me>
2025-09-15 23:19:28 +0200

    Fixed alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-      <header className="flex flex-row justify-between gap-4 pb-16 pt-8 md:pt-16 px-4 md:px-0">
+      <header className="flex flex-row justify-between gap-4 pb-16 pt-8 md:pt-16 px-4 md:px-0 items-baseline">
         <Link
           to="/"
           className="tracking-tighter bg-pastel-blue text-sm font-mono font-semibold"
         >
           <h1 className=" text-xl">Mitch Creates</h1>
         </Link>
-        <nav className="flex gap-4">
+        <nav className="flex gap-4 items-end">
           <Link
             className="tracking-tighter text-slate-950 text-sm font-mono font-semibold"
             to="/about"
